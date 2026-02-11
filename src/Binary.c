fcb5cb45986547257b26fe991c7f6e480e56fbb5
Thomastree <thomastree@126.com>
2026-02-11 12:56:43 -0800

    delete normal function

--- src/Binary.c
+++ src/Binary.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include"number_request.h"
 #include"number_determine.h"
-#include"number_calc.h"
+//#include"number_calc.h"
 #include"number_calc_arr.h"
+//the parts be commeted are no longer used
 
 int main(void){
 	int numd = getnum();
 	int digit = getdigit(numd);
 	printf("%d digits required\n", digit);
-	if(digit>10){
+//	if(digit>10){
 		printf("digit over 10, turn into array mode\n");
 		int arr[digit];
 		getans_a(digit, numd, arr);
@@ -19,10 +20,10 @@ int main(void){
 			digit=digit-1;
 		}
 		printf("\n");
-	}
-	else{
+//	}
+/*	else{
 		int ans = getans(digit, numd);
 		printf("The final answer is %d\n", ans);
-	}
+	}*/
 	return 0;	
 }
